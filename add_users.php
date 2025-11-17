fd48e69c449bb41fa5a54b88d57715fd54b4ee1c
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-11-17 11:24:56 +0000

    Update and rename add_users to add_users.php

--- add_users.php
+++ add_users.php
@@ -1,6 +1,6 @@
 <?php
 // Simple script to add 100 users - run from command line or browser
-$serverName = "tcp:mycardiffmet.database.windows.net,1433";
+$serverName = "";
 $connectionOptions = array(
     "Database" => "myDatabase",
     "Uid" => "myadmin", 
